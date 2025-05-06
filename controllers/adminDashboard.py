28cb0f9877d352f25374da87e2a92b0b1d403481
24f1001179 <24f1001179@ds.study.iitm.ac.in>
2025-05-07 00:14:29 +0530

    Implemented restricted access to user and admin routes
    Implemented restricted access to the admin and user routes (Blueprints) using the type stored in session based on login.

--- controllers/adminDashboard.py
+++ controllers/adminDashboard.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from flask import render_template, url_for, redirect
 from controllers.bp import adminbp 
+
+@adminbp.before_request
+def restrict() :
+    if session.get("type") != "admin" :
+        return redirect(url_for("general.signIn"))
 
 @adminbp.route("/dashboard", methods = ["GET"])
 def dashboard() :
--- controllers/userDashboard.py
+++ controllers/userDashboard.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from flask import render_template, url_for, redirect
 from controllers.bp import userbp 
+
+@userbp.before_request
+def restrict() :
+    if session.get("type") != "user" :
+        return redirect(url_for("general.signIn"))
 
 @userbp.route("/dashboard", methods = ["GET"])
 def dashboard() :
